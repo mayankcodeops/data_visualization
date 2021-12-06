8169639d6a64b05d562984c4282b63607cc40188
mayankcodeops <mayank@codeops.tech>
2021-12-06 19:24:44 +0530

    Converted dates to numeric format for numeric operations

--- data_visualization.py
+++ data_visualization.py
@@ -1,6 +1,8 @@
 from pandas.core.algorithms import value_counts
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 COVID_DISTRICT_WISE_URL = 'https://data.covid19india.org/state_district_wise.json'
 COVID_STATE_WISE_DAILY_URL = 'https://api.covid19india.org/states_daily.json'
@@ -17,7 +19,8 @@ covid_data = pd.json_normalize(covid_data)
 #making data usable
 df = covid_data
 
-df.date = pd.to_datetime(df.date)
+df['date'] = pd.to_datetime(df.date)
+df['dateymd'] = pd.to_datetime(df.dateymd)
 
 #taking only the confirmed cases Series records
 df = df[df.status == 'Confirmed']
@@ -28,5 +31,10 @@ df.drop('status', axis=1, inplace=True)
 #setting the date to be the index column
 df.set_index('date', inplace = True)
 
+#converting datatype to integer
+df = df.apply(pd.to_numeric)
 
+# Tabulation styling 
+df = df.tail(50)
 
+# Plotting Histogram
